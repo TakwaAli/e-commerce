16498c91cdc8dc823eb35a4744a8e0f9c6f053fd
Takwa <takwaali1520@gmail.com>
2023-04-01 21:45:13 +0200

    update & read data in details

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Womanimg from '../img/woman_hero.png'
+
 const Hero = () => {
-  return <section className='h-[800px] bg-hero bg-no-repeat bg-cover bg-centerpy-24'>
+  return <section className='h-[800px] w-[100%] bg-hero bg-no-repeat bg-cover bg-centerpy-24'>
     <div className='container mx-auto '>
      
     </div>
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React ,{useContext}from 'react';
+import{useParams}from 'react-router-dom'
+import { ProductContext } from '../contexts/ProductContext';
+import { CartContext } from '../contexts/CartContext';
 
 const ProductDetails = () => {
-  return <div>Product Details Page</div>;
+  const {id}=useParams();
+  const {products}=useContext(ProductContext)
+  const {addToCart}=useContext(CartContext)
+  const product=products.find(item=>{
+    return item.id === parseInt(id);
+  })
+  
+  return <>
+  <section className='pt-32 pb-12 lg:py-32 h-screen flex items-center'>
+    <div className='container mx-auto'>
+    <div className='flex flex-col lg:flex-row items-center'>
+    <div className='flex flex-1 justify-center items-center mb-8 lg:mb-0 '>
+      <img src={product.image}className='max-w-[180px] lg:max-w-sm' />
+    </div>
+    <div className='flex-1 text-center lg:text-left ' >
+      <h1 className='text-[26px] font-medium mb-2 max-w-[450px] max-auto '>{product.title}</h1>
+    </div>
+    <div className='text-xl text-red-500 font-medium mb-6 '>$ {product.price}</div>
+    <div>
+      <p>{product.description}</p>
+    </div>
+    </div>
+    </div>
+  </section>
+  </>;
 };
 
 export default ProductDetails;
