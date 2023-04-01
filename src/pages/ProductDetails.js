2bf1a36e82f115411536e9c7255deecbe180ed61
Takwa <takwaali1520@gmail.com>
2023-04-01 23:01:33 +0200

    update product details

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -17,14 +17,18 @@ const ProductDetails = () => {
     <div className='flex flex-col lg:flex-row items-center'>
     <div className='flex flex-1 justify-center items-center mb-8 lg:mb-0 '>
       <img src={product.image}className='max-w-[180px] lg:max-w-sm' />
-    </div>
+      </div>
     <div className='flex-1 text-center lg:text-left ' >
-      <h1 className='text-[26px] font-medium mb-2 max-w-[450px] max-auto '>{product.title}</h1>
-    </div>
+      <h1 className='text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0'>{product.title}</h1>
+   
     <div className='text-xl text-red-500 font-medium mb-6 '>$ {product.price}</div>
     <div>
-      <p>{product.description}</p>
-    </div>
+      <p className='mb-8'>{product.description}</p>
+      <button onClick={()=>addToCart(product,product.id)} className='bg-primary py-4  px-8 text-white 
+        
+      '>Add to cart</button>
+       </div>
+       </div>
     </div>
     </div>
   </section>
